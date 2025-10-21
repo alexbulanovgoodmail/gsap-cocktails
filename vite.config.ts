edea316a2ec90d1f0aa6de6a787d14dc271dcd2c
Александр Буланов <alexbulanovgoodmail@gmail.com>
2025-10-21 17:10:03 +0300

    Базовый кофинг проекта

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,34 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { checker } from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    checker({
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "./**/*.{ts,js,vue}"',
+      },
+      stylelint: {
+        lintCommand: 'stylelint "**/*.{vue,css,scss}"',
+      },
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+    css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api'],
+        additionalData: `@use "@/assets/scss/general/variables.scss" as *; @use "@/assets/scss/abstracts/mixins.scss" as *;`,
+      },
     },
   },
 })
